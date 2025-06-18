cc61130b2437a428d49e172c1702258d659bf3cc
lethimydung <lethimy.dung@petronas.com>
2025-06-18 23:11:45 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, appConfig).catch((err) =>
+//   console.error(err)
+// );
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom, inject, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { BrowserModule } from '@angular/platform-browser';
+import { HelloComponent } from './app/hello/hello.component';
+
+const run = async () => {
+  const injector = inject(Injector);
+  const el = createCustomElement(HelloComponent, { injector });
+  customElements.define('hello-element', el);
+};
+
+bootstrapApplication(HelloComponent, {
+  providers: [importProvidersFrom(BrowserModule)],
+}).then(() => run());
